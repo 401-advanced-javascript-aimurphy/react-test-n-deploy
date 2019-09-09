b581a71459f93f390891c1747781a5acf1803aee
Aileen Murphy <ai.murf2@gmail.com>
2019-09-09 15:30:51 -0700

    tests written and passing

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -6,52 +6,35 @@ import ReactDOM from 'react-dom';
 import App from '../components/app';
 import Counter from '../components/app';
 
-// return (
-//   <div>
-//     <button onClick={this.handleButtonClickDown}>LESS?</button>
-//     <h4 className={this.state.polarity}>{this.state.counter}</h4>
-//     {/* className is JSX for class */}
-//     <button onClick={this.handleButtonClickUp}>MORE!</button>
-//   </div>
-// );
+describe('<Counter/>', () => {
 
-describe('<Counter />', ()=>{
-  it('is alive at start',()=>{
-    let component=shallow(<Counter />);
-    expect(component.find('h4').exists()).toBeTruthy();
+  it('starts at 0', () => {
+    let counter = mount(<Counter />);
+    let count = counter.find('h4');
+    // button.simulate('click');
+    expect(counter.find('h4').text()).toContain('0');
   });
 
-  it('is alive at start',()=>{
-    let component=shallow(<Counter />);
-    expect(component.find('h4').exists()).toBeTruthy();
+  it('goes up on more', () => {
+    let counter = mount(<Counter />);
+    // let count = counter.find('h4');
+    let button = counter.find('#more')
+    button.simulate('click');
+    expect(counter.find('h4').text()).toContain('1');
   });
 
+  it('goes down on less', () => {
+    let counter = mount(<Counter />);
+    // let count = counter.find('h4');
+    let button = counter.find('#less')
+    button.simulate('click');
+    expect(counter.find('h4').text()).toContain('-1');
+  });
 
-})
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
-it('changes state properly', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('transfers state to DOM', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('changes state properly', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('renders correctly', () => {
+    const tree = renderer.create(<Counter />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
 // Assert state changes properly
